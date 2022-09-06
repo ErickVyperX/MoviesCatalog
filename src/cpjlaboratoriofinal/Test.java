f6a3a63a6f21a8a8d7860e1a0592bebef48d00ab
Erick Villacorta Z <erickvillacorta98@gmail.com>
2022-09-05 22:47:14 -0500

    Option validation

--- src/cpjlaboratoriofinal/Test.java
+++ src/cpjlaboratoriofinal/Test.java
@@ -1,13 +1,14 @@
 package cpjlaboratoriofinal;
 
 import com.erick.movies.business.*;
+
 import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int option;
-        String movieName;
+        String movieName, lineOption;
         MoviesCatalog moviesCatalog = new MoviesCatalogImpl();
         do {
             System.out.println("""
@@ -17,9 +18,14 @@ public class Test {
                     3- Find a movie
                     4- List all movies
                     5- Exit""");
-            option = Integer.parseInt(scanner.nextLine());
+            lineOption = scanner.nextLine();
+            option = lineOption.matches("[0-9]*")
+                    ? Integer.parseInt(lineOption)
+                    : -1;
             switch (option) {
-                case 1 -> moviesCatalog.startFile();
+                case 1 -> {
+                    moviesCatalog.startFile();
+                }
                 case 2 -> {
                     System.out.println("Enter a movie name:");
                     movieName = scanner.nextLine();
@@ -30,7 +36,10 @@ public class Test {
                     movieName = scanner.nextLine();
                     moviesCatalog.findMovie(movieName);
                 }
-                case 4 -> moviesCatalog.listMovies();
+                case 4 -> {
+                    moviesCatalog.listMovies();
+                }
+                case 5 -> System.out.println("Bye Bye!");
                 default -> System.out.println("Invalid Option!");
             }
         } while (option != 5);
